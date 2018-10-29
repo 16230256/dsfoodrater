b75192faa150247bf7d5aef6103681e7dabf3137
16230256 <39533070+16230256@users.noreply.github.com>
2018-10-29 14:10:05 +0000

    last changes still not working

--- app/src/main/java/ie/ul/deirdreshanahan/dsfoodrater/FoodAdapter.java
+++ app/src/main/java/ie/ul/deirdreshanahan/dsfoodrater/FoodAdapter.java
@@ -8,26 +8,54 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
-    public void addFood(){
+import java.util.ArrayList;
 
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
+    private List<Food> mFoods = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        mRecyclerView = recyclerView;
     }
+
+
+    public void addFood() {
+        mFoods.add(0, new Food());
+        notifyItemInserted(0);
+        notifyItemRangeChanged(0, mFoods.size());
+        mRecyclerView.getLayoutManager().scrollToPosition(0);
+        }
+        private void deleteFood(int position) {
+        mFoods.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(0,mFoods.size());
+        }
 
     @NonNull
     @Override
     public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //to do
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+        final Food food = mFoods.get(position);
+        holder.mName.setText(food.getName());
+        //todo
 
     }
 
     @Override
-    public int getItemCount() {
+    public int getItemCount()
+        {
+
+       //to do
         return 0;
     }
+
 
     class FoodViewHolder extends RecyclerView.ViewHolder {
         private ImageView mImageView;
@@ -38,6 +66,19 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
             mImageView = itemView.findViewById(R.id.food_pic);
             mName = itemView.findViewById(R.id.name);
             mRatingBar = itemView.findViewById(R.id.rating_bar);
+            //todo  Delete this food on long press
+            //done together updaate the rating for this food
+            mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    if (fromUser) {
+                        //update this foods rating
+                        Food currentFood = mFoods.get(getAdapterPosition());
+                        currentFood.setRating(rating);
+                    }
+
+                }
+            });
 
         }
     }
